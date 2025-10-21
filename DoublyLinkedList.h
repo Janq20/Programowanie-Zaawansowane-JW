127112115fa507e5911557ac572c417e59c51f9f
Janek W <89817308+Janq20@users.noreply.github.com>
2025-10-21 19:36:04 +0200

    Dodanie struktury wezla

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <iostream>
+#include "Node.h"
 
 struct Node {
     int data;
     Node* prev;
     Node* next;
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
 
     Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+    ~DoublyLinkedList();
+
+    void displayForward();
 };
