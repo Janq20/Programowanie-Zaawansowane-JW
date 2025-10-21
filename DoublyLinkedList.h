eef9bb111e0dfb731f695590c90a971d2436ccb7
Janek W <89817308+Janq20@users.noreply.github.com>
2025-10-21 19:39:47 +0200

    Implementacja funkcji wyswietlania listy w przód

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,20 +1,24 @@
-#pragma once
-#include <iostream>
-#include "Node.h"
 
-struct Node {
-    int data;
-    Node* prev;
-    Node* next;
+#include "Node.h"
+#include "DoublyLinkedList.h"
+#include <iostream>
+
 class DoublyLinkedList {
 private:
     Node* head;
     Node* tail;
 
-    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
 public:
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
     ~DoublyLinkedList();
 
     void displayForward();
 };
+void DoublyLinkedList::displayForward() {
+    Node* temp = head;
+    while (temp) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << "\n";
+}
